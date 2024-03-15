0e52fc82730302b09fa9c12853563f11f1dbf69a
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2024-03-15 15:44:40 +0900

    add Subtitle component

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from './Button';
 export { default as Icon } from './Icon';
 export { default as IconButton } from './IconButton';
 export { default as Title } from './Title';
+export { default as Subtitle } from './Subtitle';
