6029d1d716bb60633661f06b67f5c19bf4b758a8
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2024-03-15 16:15:02 +0900

    add Div component

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,3 +3,4 @@ export { default as Icon } from './Icon';
 export { default as IconButton } from './IconButton';
 export { default as Title } from './Title';
 export { default as Subtitle } from './Subtitle';
+export { default as Div } from './Div';
