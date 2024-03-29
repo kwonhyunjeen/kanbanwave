4ceae0a9e5420cccc35610ff7936d5383022378d
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2024-03-29 18:08:14 +0900

    rename useBackdropClick to useOnOutsideClick

---
+++ src/hooks/useOnOutsideClick.ts
@@ -0,0 +1,21 @@
+import { RefObject, useEffect } from 'react';
+
+const useOnOutsideClick = (
+  ref: RefObject<HTMLElement>,
+  callback?: (event: Event) => void
+) => {
+  useEffect(() => {
+    const handleOutsideClick = (e: Event): void => {
+      if (ref.current === null || ref.current.contains(e.target as Node)) {
+        return;
+      }
+      callback?.(e);
+    };
+    window.addEventListener('mousedown', handleOutsideClick);
+    return () => {
+      window.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [ref, callback]);
+};
+
+export default useOnOutsideClick;
