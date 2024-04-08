edfbe9aa65478314501f6a1de7f3ff479342c60f
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2024-04-09 01:37:59 +0900

    add Board page and route

---
+++ client/src/pages/Board.tsx
@@ -0,0 +1,5 @@
+const Board = () => {
+  return <div>Board Page</div>;
+};
+
+export default Board;
--- client/src/routes/router.tsx
+++ client/src/routes/router.tsx
@@ -1,10 +1,12 @@
 import { BaseLayout } from 'components';
+import { Board } from 'pages';
 import { createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <BaseLayout />
+    element: <BaseLayout />,
+    children: [{ path: '/board/:id/:title', element: <Board /> }]
   }
 ]);
 
