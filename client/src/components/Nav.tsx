e67bfc49943723b09f1d6ecae8aa658dea79599e
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2024-04-15 16:17:58 +0900

    fix Nav component styles

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -7,51 +7,63 @@ type NavProps = {
 };
 
 const Nav = ({ open, onClickDrawer }: NavProps) => {
+  const drawerIconStyle = 'btn-xs btn-ghost btn-square';
   return (
     <nav
       className={clsx('app-nav', {
-        'translate-x-0': !open,
+        'translate-x-0 ': !open,
         '-translate-x-[15rem]': open
       })}>
-      <div className="flex items-center h-16 p-4 border-b">
-        <Icon name="cruelty_free" className="mr-2 text-3xl" />
-        <p className="flex-1">Trello workspace</p>
-        <IconButton
-          name="double_arrow"
-          onClick={onClickDrawer}
-          className={clsx('btn-xs btn-ghost btn-square', {
-            'fixed -right-4 bg-transparent border-0 shadow-none': open,
-            'rotate-180': !open
-          })}
-        />
-      </div>
-      <ul className="bg-white menu">
-        <li>
-          <a>
-            <Icon name="dashboard" />
-            Boards
-          </a>
-        </li>
-        <li>
-          <a>
-            <Icon name="person" />
-            Members
-          </a>
-        </li>
-        <li>
-          <details open className="transition duration-300 ease-in-out">
-            <summary className="font-semibold">Your boards</summary>
-            <ul className="transition-all duration-300 ease-in-out">
-              <li>
-                <a>Untitled board 1</a>
-              </li>
-              <li>
-                <a>Untitled board 2</a>
-              </li>
-            </ul>
-          </details>
-        </li>
-      </ul>
+      {!open ? (
+        <>
+          <div className="flex items-center h-16 p-4 border-b">
+            <Icon name="cruelty_free" className="mr-2 text-3xl" />
+            <p className="flex-1">Trello workspace</p>
+            <IconButton
+              name="double_arrow"
+              onClick={onClickDrawer}
+              className={clsx(drawerIconStyle, 'rotate-180')}
+            />
+          </div>
+          <ul className="menu">
+            <li>
+              <a>
+                <Icon name="dashboard" />
+                Boards
+              </a>
+            </li>
+            <li>
+              <a>
+                <Icon name="person" />
+                Members
+              </a>
+            </li>
+            <li>
+              <details open>
+                <summary className="font-semibold">Your boards</summary>
+                <ul>
+                  <li>
+                    <a>Untitled board 1</a>
+                  </li>
+                  <li>
+                    <a>Untitled board 2</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </>
+      ) : (
+        <div className="absolute h-full bg-stone-400 -right-1" onClick={onClickDrawer}>
+          <IconButton
+            name="double_arrow"
+            className={clsx(
+              drawerIconStyle,
+              'w-6 h-full  hover:bg-stone-400 border-0 shadow-none rounded-none'
+            )}
+          />
+        </div>
+      )}
     </nav>
   );
 };
