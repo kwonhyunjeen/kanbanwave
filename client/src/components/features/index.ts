c2d2e878892e9c6447710229fe14caa034f938a6
kwonhyunjeen <157392752+kwonhyunjeen@users.noreply.github.com>
2024-06-08 01:06:16 +0900

    Refactor: refactor DB(localStorage) structure and DnD logic (#15)
    * add StrictModeDroppable component
    * replace 'react-dnd' with 'react-beautiful-dnd'
    * remove 'react-dnd'
    * refactor DB(localStorage) structure

--- client/src/components/features/index.ts
+++ client/src/components/features/index.ts
@@ -1,10 +1,9 @@
 export { default as AddItemForm } from './AddItemForm';
 export { default as CardDraggable } from './CardDraggable';
 export { default as CardDroppable } from './CardDroppable';
-export { default as ListDraggable } from './ListDraggable';
-export { default as ListDroppable } from './ListDroppable';
 export { default as Card } from './Card';
 export { default as List } from './List';
+export { default as StrictModeDroppable } from './StrictModeDroppable';
 export {
   default as KanbanStorageProvider,
   useKanbanBoard,
--- client/src/pages/Board.tsx
+++ client/src/pages/Board.tsx
@@ -1,14 +1,13 @@
 import {
   AddItemForm,
-  ListDroppable,
   Title,
   List,
   useKanbanList,
-  useKanbanCard
+  useKanbanCard,
+  StrictModeDroppable
 } from 'components';
-import { useCallback, useRef } from 'react';
+import { useCallback } from 'react';
 import * as Dummy from 'dummy';
-import { useDrop } from 'react-dnd';
 import { KWItemType } from 'store';
 import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 import { useLocation } from 'react-router-dom';
@@ -19,12 +18,6 @@ const Board = () => {
   const boardTitle = location.state?.board?.title;
   const boardId = location.state?.board?.id;
 
-  const divRef = useRef<HTMLDivElement>(null);
-  const [, drop] = useDrop({
-    accept: KWItemType.LIST
-  });
-  drop(divRef);
-
   const listStore = useKanbanList();
   const cardStore = useKanbanCard();
   const lists = listStore.getAll(boardId);
@@ -34,9 +27,8 @@ const Board = () => {
       const id = Dummy.randomUUID();
       const list = { id, title };
       listStore.create(boardId, list);
-      cardStore.reorder(list.id, []);
     },
-    [listStore, cardStore, boardId]
+    [listStore, boardId]
   );
 
   const handleListDelete = useCallback(
@@ -46,86 +38,55 @@ const Board = () => {
     [listStore, boardId]
   );
 
-  const handleListMove = useCallback(
-    (dragIndex: number, hoverIndex: number) => {
-      const newOrders = [...lists];
-      const [draggedItem] = newOrders.splice(dragIndex, 1); // 드래그된 아이템 제거
-      newOrders.splice(hoverIndex, 0, draggedItem); // 새로운 위치에 아이템 추가
-
-      listStore.reorder(
-        boardId,
-        newOrders.map(list => list.id)
-      );
-    },
-    [listStore, lists, boardId]
-  );
-
   const handleDragEnd = useCallback(
     (result: DropResult) => {
-      const droppableIdListId = result.destination?.droppableId;
-      const droppableIdCardIndex = result.destination?.index;
-      if (droppableIdListId === undefined || droppableIdCardIndex === undefined) {
-        return;
-      }
+      const { type, draggableId, source, destination } = result;
+      if (!destination) return;
 
-      const draggableListId = result.source.droppableId;
-      const draggableCardIndex = result.source.index;
-
-      // 같은 목록에서 카드 옮길 때: 두 카드의 index 교체
-      if (droppableIdListId === draggableListId) {
-        const cardIdOrders = cardStore.getOrders(droppableIdListId);
+      if (type === KWItemType.LIST) {
+        listStore.reorder(destination.droppableId, draggableId, destination.index);
+      } else if (type === KWItemType.CARD) {
         cardStore.reorder(
-          droppableIdListId,
-          cardIdOrders.map((item, index) =>
-            index === draggableCardIndex
-              ? cardIdOrders[droppableIdCardIndex]
-              : index === droppableIdCardIndex
-              ? cardIdOrders[draggableCardIndex]
-              : item
-          )
+          source.droppableId,
+          destination.droppableId,
+          draggableId,
+          destination.index
         );
-        // 다른 목록으로 카드 옮길 때: 기존 리스트에서 카드 uuid 삭제, 드롭 리스트에서 카드 uuid 추가
-      } else {
-        const draggableCardIdOrders = cardStore.getOrders(draggableListId);
-        cardStore.reorder(
-          draggableListId,
-          draggableCardIdOrders.filter((notUsed, index) => index !== draggableCardIndex)
-        );
-        const droppableIdCardIdOrders = cardStore.getOrders(droppableIdListId);
-        cardStore.reorder(droppableIdListId, [
-          ...droppableIdCardIdOrders.slice(0, droppableIdCardIndex),
-          result.draggableId,
-          ...droppableIdCardIdOrders.slice(droppableIdCardIndex)
-        ]);
       }
     },
-    [cardStore]
+    [listStore, cardStore]
   );
 
   return (
     <section className="app-base">
       <Title className="mb-4 text-white">{boardTitle}</Title>
       <DragDropContext onDragEnd={handleDragEnd}>
-        <ListDroppable>
-          <div className="flex justify-start">
-            <div className="flex">
-              {lists?.map((list, index) => (
+        <StrictModeDroppable
+          droppableId={boardId}
+          direction="horizontal"
+          type={KWItemType.LIST}>
+          {provided => (
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className="flex justify-start">
+              {lists.map((list, index) => (
                 <List
                   key={list.id}
                   list={list}
                   index={index}
-                  onListMove={handleListMove}
                   onListDelete={handleListDelete(list.id)}
                 />
               ))}
+              {provided.placeholder}
+              <AddItemForm
+                itemMode="list"
+                onItemAdd={handleListAdd}
+                listsLength={lists.length}
+              />
             </div>
-            <AddItemForm
-              itemMode="list"
-              onItemAdd={handleListAdd}
-              listsLength={lists.length}
-            />
-          </div>
-        </ListDroppable>
+          )}
+        </StrictModeDroppable>
       </DragDropContext>
     </section>
   );
