53345e5eb8ae993b90d73cc3b717719bef10b638
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2024-06-16 03:21:18 +0900

    refactor storage API spec

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,92 +14,41 @@ import {
   KanbanStorageProvider,
   KanbanwaveStorage
 } from 'kanbanwave';
-import { DB } from 'app/utils';
+import * as API from 'app/api';
 
 export const sampleKanbanStorage: KanbanwaveStorage = {
-  getBoards: () => DB.getBoards(),
+  getBoards: () => API.getBoards(),
 
-  getBoardContent: (boardId: KWBoardUUID) => ({
-    ...DB.getBoards([boardId])[0],
-    lists: DB.getLists(DB.getListOrdersByBoardId(boardId)).map(list => ({
-      ...list,
-      cards: DB.getCards(DB.getCardOrdersByListId(list.id))
-    }))
-  }),
+  getBoardContent: (boardId: KWBoardUUID) => API.getBoardContent(boardId),
 
-  createBoard: (board: KWBoardForm) => {
-    DB.setBoards([...DB.getBoards(), { ...board, id: crypto.randomUUID() }]);
-  },
+  createBoard: (board: KWBoardForm) => API.createBoard(board),
 
-  updateBoard: (board: KWBoard) => {
-    DB.setBoards(DB.getBoards().map(it => (it.id === board.id ? board : it)));
-  },
+  updateBoard: (board: KWBoard) => API.updateBoard(board),
 
-  deleteBoard: (boardId: KWBoardUUID) => {
-    DB.removeBoards([boardId]);
-    const listIds = DB.getListOrdersByBoardId(boardId);
-    DB.removeLists(listIds);
-    listIds.forEach(listId => {
-      const cardIds = DB.getCardOrdersByListId(listId);
-      DB.removeCards(cardIds);
-    });
-  },
+  deleteBoard: (boardId: KWBoardUUID) => API.deleteBoard(boardId),
 
-  createList: (boardId: KWBoardUUID, list: KWListForm) => {
-    const listId = crypto.randomUUID();
-    DB.setLists([...DB.getLists(), { ...list, id: listId, boardId }]);
-    DB.setListOrdersByBoardId(boardId, [...DB.getListOrdersByBoardId(boardId), listId]);
-  },
+  createList: (boardId: KWBoardUUID, list: KWListForm) => API.createList(boardId, list),
 
-  updateList: (boardId: KWBoardUUID, list: KWList) => {
-    DB.setLists(DB.getLists().map(it => (it.id === list.id ? list : it)));
-  },
+  updateList: (boardId: KWBoardUUID, list: KWList) => API.updateList(list),
 
-  deleteList: (boardId: KWBoardUUID, listId: KWListUUID) => {
-    DB.setListOrdersByBoardId(
-      boardId,
-      DB.getListOrdersByBoardId(boardId).filter(id => id !== listId)
-    );
-    DB.removeLists([listId]);
-    const cardIds = DB.getCardOrdersByListId(listId);
-    DB.removeCardOrdersByListId(listId);
-    DB.removeCards(cardIds);
-  },
+  deleteList: (boardId: KWBoardUUID, listId: KWListUUID) => API.deleteList(listId),
 
   reorderList: (
     boardId: KWBoardUUID,
     draggedListId: KWListUUID,
     droppedListIndex: number
-  ) => {
-    const listOrders = DB.getListOrdersByBoardId(boardId);
-    const draggedListIndex = listOrders.findIndex(id => id === draggedListId);
-    if (draggedListIndex === -1) {
-      throw new Error(`List not found by '${draggedListId}'.`);
-    }
-    listOrders.splice(draggedListIndex, 1);
-    listOrders.splice(droppedListIndex, 0, draggedListId);
-    DB.setListOrdersByBoardId(boardId, listOrders);
-  },
+  ) => API.reorderList(boardId, draggedListId, droppedListIndex),
 
-  getCard: (cardId: KWCardUUID) => DB.getCards([cardId])[0],
+  getCard: (cardId: KWCardUUID) => API.getCard(cardId),
 
-  createCard: (boardId: KWBoardUUID, listId: KWListUUID, card: KWCardForm) => {
-    const cardId = crypto.randomUUID();
-    DB.setCards([...DB.getCards(), { ...card, id: cardId, boardId, listId }]);
-    DB.setCardOrdersByListId(listId, [...DB.getCardOrdersByListId(listId), cardId]);
-  },
+  createCard: (boardId: KWBoardUUID, listId: KWListUUID, card: KWCardForm) =>
+    API.createCard(listId, card),
 
-  updateCard: (boardId: KWBoardUUID, listId: KWListUUID, card: KWCard) => {
-    DB.setCards(DB.getCards().map(it => (it.id === card.id ? card : it)));
-  },
+  updateCard: (boardId: KWBoardUUID, listId: KWListUUID, card: KWCard) =>
+    API.updateCard(card),
 
-  deleteCard: (boardId: KWBoardUUID, listId: KWListUUID, cardId: KWCardUUID) => {
-    DB.setCardOrdersByListId(
-      listId,
-      DB.getCardOrdersByListId(listId).filter(id => id !== cardId)
-    );
-    DB.removeCards([cardId]);
-  },
+  deleteCard: (boardId: KWBoardUUID, listId: KWListUUID, cardId: KWCardUUID) =>
+    API.deleteCard(cardId),
 
   reorderCard: (
     boardId: KWBoardUUID,
@@ -107,23 +56,7 @@ export const sampleKanbanStorage: KanbanwaveStorage = {
     toListId: KWListUUID,
     draggedCardId: KWCardUUID,
     droppedCardIndex: number
-  ) => {
-    console.log({ fromListId, toListId, draggedCardId, droppedCardIndex });
-    const isSameList = fromListId === toListId;
-    const cardOrdersFromList = DB.getCardOrdersByListId(fromListId);
-    // from과 to가 같다면, cardOrders는 같은 객체를 참조함
-    const cardOrdersToList = isSameList
-      ? cardOrdersFromList
-      : DB.getCardOrdersByListId(toListId);
-    const draggedCardIndex = cardOrdersFromList.findIndex(id => id === draggedCardId);
-    if (draggedCardIndex === -1) {
-      throw new Error(`Card not found by '${draggedCardId}'.`);
-    }
-    cardOrdersFromList.splice(draggedCardIndex, 1);
-    cardOrdersToList.splice(droppedCardIndex, 0, draggedCardId);
-    if (!isSameList) DB.setCardOrdersByListId(fromListId, cardOrdersFromList);
-    DB.setCardOrdersByListId(toListId, cardOrdersToList);
-  }
+  ) => API.reorderCard(fromListId, toListId, draggedCardId, droppedCardIndex)
 };
 
 function KanbanProvider({ children }: PropsWithChildren) {
