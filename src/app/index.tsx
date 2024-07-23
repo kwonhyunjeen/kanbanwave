6b61dd24dc63c9fb6b372751ed7d10e6911ec5c7
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2024-07-23 17:29:09 +0900

    set the style to 'kanbanwave'

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import reportWebVitals from './reportWebVitals';
-import '@fontsource/material-icons';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
