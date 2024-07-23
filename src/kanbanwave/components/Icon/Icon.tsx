e3bf7977852bfe2077fb6df83df557d5963f9e38
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2024-07-23 17:29:30 +0900

    add `Icon`, `Button` and `IconButton` components and styles to 'kanbanwave'

---
+++ src/kanbanwave/components/Icon/Icon.tsx
@@ -0,0 +1,39 @@
+import clsx from 'clsx';
+import { forwardRef } from 'react';
+import styles from './Icon.module.css';
+
+type IconProps = React.ComponentPropsWithoutRef<'span'> & {
+  name: string;
+  size?: 'sm' | 'md' | 'lg';
+};
+
+const Icon = forwardRef<HTMLSpanElement, IconProps>((props, ref) => {
+  const {
+    'aria-hidden': hidden,
+    'aria-label': label,
+    className,
+    name,
+    size = 'md',
+    ...rest
+  } = props;
+
+  const iconClass = clsx(
+    styles.root,
+    styles[`icon${size.charAt(0).toUpperCase() + size.slice(1)}`],
+    className
+  );
+
+  return (
+    <span
+      {...rest}
+      ref={ref}
+      className={iconClass}
+      aria-hidden={hidden ?? label ? undefined : true}
+      aria-label={label}>
+      {name}
+    </span>
+  );
+});
+
+Icon.displayName = 'Icon';
+export default Icon;
---
+++ src/kanbanwave/components/IconButton/IconButton.tsx
@@ -0,0 +1,42 @@
+import { forwardRef } from 'react';
+import clsx from 'clsx';
+import Button from '../Button/Button';
+import styles from './IconButton.module.css';
+import Icon from '../Icon/Icon';
+
+type IconButtonProps = Omit<React.ComponentPropsWithoutRef<typeof Button>, 'size' | 'startIcon' | 'endIcon' | 'children'> & {
+  icon: string;
+  size?: 'sm' | 'md' | 'lg';
+}
+
+const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>((props, ref) => {
+  const {
+    className,
+    color = 'primary',
+    icon,
+    size = 'sm',
+    variant = 'text',
+    ...rest
+  } = props;
+
+  const buttonClass = clsx(
+    styles.root,
+    styles[`size${size.charAt(0).toUpperCase() + size.slice(1)}`],
+    className
+  );
+
+  return (
+    <Button
+      {...rest}
+      ref={ref}
+      className={buttonClass}
+      color={color}
+      variant={variant}
+      size={size}>
+      <Icon name={icon} size={size} />
+    </Button>
+  );
+});
+
+IconButton.displayName = 'IconButton';
+export default IconButton;
