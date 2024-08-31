8ec21022029b9acc04b6d45569c09da757b8413b
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2024-08-31 16:58:44 +0900

    set up eslint in each directory

--- packages/kanbanwave/eslint.config.mjs
+++ packages/kanbanwave/eslint.config.mjs
@@ -1,12 +1,54 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
+import jseslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import unusedImports from 'eslint-plugin-unused-imports';
 
-export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended
-];
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    }
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    extends: [
+      jseslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      jsxA11y.flatConfigs.recommended,
+      react.configs.flat.recommended
+    ],
+    plugins: {
+      'react-hooks': reactHooks,
+      'unused-imports': unusedImports
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+
+      'unused-imports/no-unused-imports': 'error',
+
+      '@typescript-eslint/no-restricted-imports': [
+        'error',
+        {
+          paths: [
+            {
+              name: 'react',
+              importNames: ['default'],
+              message: '`React` is already a global variable.'
+            }
+          ]
+        }
+      ],
+
+      'react/react-in-jsx-scope': 'off'
+    }
+  }
+);
